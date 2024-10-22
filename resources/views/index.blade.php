e4d54ff395627d1f76913e736486aa2d31d185e4
mariovf <mariovalfid@gmail.com>
2024-10-22 14:53:20 +0200

    Arreglo env y datos de app

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mt-5">
+        <h1>Bienvenido a Gesluz</h1>
+        <p>Esta es la página de inicio de Gesluz.</p>
+    </div>
+@endsection
