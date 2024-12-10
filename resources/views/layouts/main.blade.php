bb3be3b0af6eb2ad050644d7bbe43f0793782401
mariovf <mavafi.inf@aquacorp.es>
2024-12-10 17:57:12 +0100

    Arreglos

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <!-- Estilos adicionales -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- <link rel="stylesheet" href="/resources/css/app.css"> -->
 </head>
 <body>
     <!-- Encabezado / Navbar -->
